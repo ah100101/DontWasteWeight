003c0cf0ccda8fe55dd67513b7480263f8c092bd
Alex Hawley <ahawley37@gmail.com>
2015-08-10 23:25:17 -0500

    added abstract class for bestfirstsearch, added interface for bestfirstsearchable

---
+++ Axel/Algorithms/Search/BestFirstSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axel.Algorithms.Search
+{
+    //
+    // Summary:
+    //      Represents a variable size Best First Search (A* Search) Algorithm for find best path to solution
+    //
+    // Type parameters:
+    //   T:
+    //     Specifies the type of elements in the stack. Must implement BestFirstSearchable
+    public class BestFirstSearch<T>
+    {
+    }
+}
